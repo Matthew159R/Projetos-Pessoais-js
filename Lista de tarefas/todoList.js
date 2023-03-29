54767f323b5164b0c8115f0877dd8df5c9c9ad2e
Matthew159R <117731536+Matthew159R@users.noreply.github.com>
2023-03-29 12:47:36 -0300

    Um todoList feito com js puro
    Esse todo list foi feito com js puro,  ainda tem mais algumas coisas para colocar, mas assim já está bom

---
+++ Lista de tarefas/todoList.js
@@ -0,0 +1,55 @@
+const formAddTodo = document.querySelector('.form-add-todo')
+const todosContainer = document.querySelector('.todos-container')
+const inputSearch = document.querySelector('.form-search input')
+
+
+formAddTodo.addEventListener('submit', event => {
+    event.preventDefault()
+
+    const inputValue = event.target.add.value.trim()
+    if(inputValue.length) {
+        todosContainer.innerHTML += `
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <span>${inputValue}</span>
+            <i class="far fa-trash-alt delete"></i>
+          </li>
+        `
+        event.target.reset()
+        
+        localStorage.setItem('item', inputValue)
+    }
+})
+
+todosContainer.addEventListener('click', event => {
+    const clickedElement = event.target
+
+    if (Array.from(clickedElement.classList).includes('delete')) {
+        const li = clickedElement.parentElement
+        li.innerHTML = `<s>${li.textContent}</s>`
+        li.style.color = '#ea868f'
+        li.style.backgroundColor = '#2c0b0e'
+        setTimeout(() => {
+            li.remove()
+        }, 1000)
+        
+    }
+})
+
+inputSearch.addEventListener('input', event => {
+    const inputValue = event.target.value.trim().toLowerCase()
+    Array.from(todosContainer.children)
+    .filter(todo => !todo.textContent.toLowerCase().includes(inputValue))
+    .forEach(todo => {
+        todo.classList.remove('d-flex')
+        todo.classList.add('hidden')
+    })
+    Array.from(todosContainer.children)
+    .filter(todo => todo.textContent.toLowerCase().includes(inputValue))
+    .forEach(todo => {
+        todo.classList.remove('hidden')
+        todo.classList.add('d-flex')
+    })
+})
+
+
+
