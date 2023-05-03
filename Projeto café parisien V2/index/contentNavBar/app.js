1d1e409c06124e274574cc22a0f106a6ca640caa
Matthew159R <117731536+Matthew159R@users.noreply.github.com>
2023-05-03 10:32:05 -0300

    Add files via upload

---
+++ Projeto café parisien V2/index/contentNavBar/app.js
@@ -0,0 +1,79 @@
+const carousel = document.querySelector('.carousel')
+const next = document.querySelector('.next')
+const back = document.querySelector('.back')
+
+let indexImg = 0
+
+const imgs = Array.from(carousel.children)
+
+next.addEventListener('click', event => {
+    indexImg += 1
+  
+    if (indexImg > 4) {
+        indexImg = 0
+    }else if (indexImg < 0){
+        indexImg = 4
+    }
+
+    imgs.forEach((img, index) => {
+        
+        if (indexImg === index) {
+            if (indexImg === 0) {
+                imgs[0].classList.remove('none')
+                imgs[0].style.animation = 'fadeVisibility 0.5s'    
+                imgs[4].classList.add('none')
+                imgs[4].style.animation = ''
+
+            }else {
+                imgs[indexImg - 1].setAttribute('class', 'none')
+                imgs[indexImg - 1].style.animation = ''
+                imgs[indexImg].classList.remove('none')
+
+                imgs[indexImg].style.animation = 'fadeVisibility 0.5s'
+                console.log(indexImg, img)
+            }
+        }
+    })
+})
+
+back.addEventListener('click', event => {
+    indexImg -= 1
+  
+    if (indexImg > 4) {
+        indexImg = 0
+    }else if (indexImg < 0){
+        indexImg = 4
+    }
+
+    imgs.forEach((img, index) => {
+        
+        if (indexImg === index) {
+            if (indexImg === 4) {
+                imgs[4].classList.remove('none')
+                imgs[4].style.animation = 'fadeVisibility 0.5s'    
+                imgs[0].classList.add('none')
+                imgs[0].style.animation = ''
+
+                console.log(indexImg, img)
+            }else {
+                imgs[indexImg + 1].setAttribute('class', 'none')
+                imgs[indexImg + 1].style.animation = ''
+                imgs[indexImg].classList.remove('none')
+
+                imgs[indexImg].style.animation = 'fadeVisibility 0.5s'
+                console.log(indexImg, img)
+            }
+        }
+    })
+})
+
+setInterval(() => {
+    next.click()
+}, 4000)
+
+const buttonBuy = document.querySelector('button')
+
+buttonBuy.addEventListener('mouseleave', event => {
+    buttonBuy.style.animation = 'buyAnimationReverse 0.5s forwards'
+    buttonBuy.style.border = 'solid 1px orange'
+})
